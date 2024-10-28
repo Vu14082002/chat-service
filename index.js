46c44b59ebd8cdea2d055b3ee440022c0dd28a39
Vu14082002 <nguyenvanvu20020814@gmail.com>
2024-10-28 14:13:19 +0700

    feat: api payment and event payment

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ io.on('connection', (socket) => {
   console.log('Socket connected: ', socket.id);
   socketServer(socket, io);
 });
+
+app.post('/api/payment', (req, res) => {
+  const { userId, isSuccess } = req.body;
+  if (!userId || !isSuccess) {
+    return res.status(400).json({ error: 'missing userId or status or status ' });
+  }
+  let messageText =
+    isSuccess === true ? 'Thanh toán thành công' : 'Thanh toán thất bại, vui lòng thử lại';
+  let message = {
+    status: isSuccess,
+    message: messageText,
+  };
+  io.to(userId).emit('payment', message);
+  return res.status(200).json({ success: true, message: 'Thông báo đã được gửi qua Socket' });
+});
